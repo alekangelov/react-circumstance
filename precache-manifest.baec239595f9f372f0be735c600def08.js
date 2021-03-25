3c7e3c9cbcd87ed47ac5edfd9fd76822e3be93fc
Alek Angelov <alek.angelov.sk@gmail.com>
2021-03-25 01:48:47 +0100

    Updates

---
+++ precache-manifest.baec239595f9f372f0be735c600def08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74fc069f8c15b5b13da10386cb512c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ecc30211eb403ea58710",
+    "url": "./static/css/main.88c257a1.chunk.css"
+  },
+  {
+    "revision": "aee54cb90dfa8e75b11d",
+    "url": "./static/js/2.3203c9c9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3203c9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc30211eb403ea58710",
+    "url": "./static/js/main.b53110f4.chunk.js"
+  },
+  {
+    "revision": "8d1622e7822d8aaaf8af",
+    "url": "./static/js/runtime-main.f867a6fe.js"
+  },
+  {
+    "revision": "cd7059412e060fcf9f8a2235ec09bfe8",
+    "url": "./static/media/tux-batman.cd705941.webp"
+  },
+  {
+    "revision": "4ab6ee995314db94a295ce528aa871b5",
+    "url": "./static/media/tux-spiderman.4ab6ee99.png"
+  }
+]);
